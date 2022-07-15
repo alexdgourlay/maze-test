23b698e1feb9196eb9a2587f1abad0b9a967f24a
Alex Gourlay <alexdgourlay@gmail.com>
2022-07-15 16:20:42 +0100

    Update config.tf

--- dsd/config.tf
+++ dsd/config.tf
@@ -7,25 +7,31 @@ provider aws {
 
 resource aws_s3_bucket _42057 {
 provider = aws.aws-1
+}
 
 
 resource aws_security_group _43079 {
 provider = aws.aws-1
+}
 
 
 resource aws_default_vpc _43077 {
 provider = aws.aws-1
+}
 
 
 resource aws_vpc _43078 {
 provider = aws.aws-1
+}
 
 
 resource aws_security_group _43080 {
 provider = aws.aws-1
+}
 
 
 resource aws_ecr_repository _43074 {
 provider = aws.aws-1
+}
 
 
